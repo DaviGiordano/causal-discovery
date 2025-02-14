8633081e550728d91f9bf8e2d2121236abce08c3
DaviGiordano <davigiordano2704@gmail.com>
2025-02-14 16:06:23 +0000

    Add verbose redirect to logs

--- utils/logging.py
+++ utils/logging.py
@@ -1,6 +1,8 @@
 import logging
 from pathlib import Path
 from datetime import datetime
+from contextlib import contextmanager
+import sys
 
 
 def setup_logging(algorithm_tag: str, data_tag: str):
@@ -25,3 +27,25 @@ def setup_logging(algorithm_tag: str, data_tag: str):
     logger.info(f"Results will be saved in {log_dir}")
 
     return logger, log_dir
+
+
+@contextmanager
+def redirect_stdout_to_logger():
+    class LoggerWriter:
+        def __init__(self):
+            # Use the same logger as setup in setup_logging()
+            self.logger = logging.getLogger(__name__)
+
+        def write(self, message):
+            if message and not message.isspace():
+                self.logger.info(message.strip())
+
+        def flush(self):
+            pass
+
+    stdout = sys.stdout
+    sys.stdout = LoggerWriter()
+    try:
+        yield
+    finally:
+        sys.stdout = stdout
