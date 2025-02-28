2619e6fde82bf5d167159cc1f76907354b7eb3b4
davigiordano <davigiordano2704@gmail.com>
2025-02-28 11:31:31 +0000

    Add dataset configs

--- scripts/run_experiments.bash
+++ scripts/run_experiments.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # List of data tags
-# data_tags=("csuite_cat_chain" "csuite_cat_collider" "csuite_cat_to_cts" "csuite_cts_to_cat" "csuite_large_backdoor" "csuite_large_backdoor_binary_t" "csuite_linexp" "csuite_lingauss" "csuite_mixed_confounding" "csuite_mixed_simpson" "csuite_nonlingauss" "csuite_nonlin_simpson" "csuite_symprod_simpson" "csuite_weak_arrows" "csuite_weak_arrows_binary_t")
-data_tags=("csuite_cts_to_cat")
+data_tags=("csuite_cat_collider" "csuite_cat_to_cts" "csuite_cts_to_cat" "csuite_large_backdoor" "csuite_large_backdoor_binary_t" "csuite_linexp" "csuite_lingauss" "csuite_mixed_confounding" "csuite_mixed_simpson" "csuite_nonlingauss" "csuite_nonlin_simpson" "csuite_symprod_simpson" "csuite_weak_arrows" "csuite_weak_arrows_binary_t" "ruta_synth_uniform" "ruta_synth_normal")
 # List of algorithms
-causallearn_algorithms=("pc_fisherz_005" "pc_fisherz_01" "pc_kcigaussian_005" "pc_kcigaussian_01" "fci_fisherz_005" "fci_fisherz_01" "fci_kcigaussian_005" "fci_kcigaussian_01" "ges_bic" "ges_bdeu" "ges_margigeneral" "ges_margimulti" "grasp_bic" "grasp_bdeu" "grasp_margigeneral" "grasp_margimulti" "boss_bic" "boss_bdeu" "boss_margigeneral" "boss_margimulti" "directlingam_pwling" "directlingam_kernel")
+causallearn_algorithms=("pc_fisherz_005" "pc_fisherz_01" "pc_kcigaussian_005" "pc_kcigaussian_01" "fci_fisherz_005" "fci_fisherz_01" "fci_kcigaussian_005" "fci_kcigaussian_01" "ges_bic" "ges_bdeu" "ges_margigeneral" "ges_margimulti" "grasp_bic" "grasp_bdeu" "grasp_margigeneral" "grasp_margimulti" "directlingam_pwling" "directlingam_kernel")
 castle_algorithms=("grandag_default" "dag_gnn_default" "corl_default" "notears_default")
 # algorithms=("${castle_algorithms[@]}" "${causallearn_algorithms[@]}") 
 
