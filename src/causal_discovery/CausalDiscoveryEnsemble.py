d74d311d2f2ffbb2be647d5332b6cfb5b8573615
DaviGiordano <davigiordano2704@gmail.com>
2025-03-20 22:28:22 +0000

    Feat: directed union and filter

--- src/causal_discovery/CausalDiscoveryEnsemble.py
+++ src/causal_discovery/CausalDiscoveryEnsemble.py
@@ -23,6 +23,8 @@ class CausalDiscoveryEnsemble:
         self.avg_edge_adj: np.ndarray = np.array([])
         self.est_skeleton_union: GeneralGraph = GeneralGraph([])
         self.edge_frequency: Dict[Tuple[int, int], str] = {}
+        self.est_directed_union: GeneralGraph = GeneralGraph([])
+        self.directed_frequency: Dict[Tuple[int, int], str] = {}
 
     def train(self, data: np.ndarray):
         for algo in self.algorithms:
@@ -30,6 +32,9 @@ class CausalDiscoveryEnsemble:
         self.is_ensemble_trained = True
         self.avg_edge_adj = self._get_avg_edge_adj()
         self.est_skeleton_union, self.edge_frequency = self._get_est_skeleton_union()
+        self.est_directed_union, self.directed_frequency = (
+            self._get_est_directed_union()
+        )
 
     def _get_avg_edge_adj(self):
         if not self.algorithms:
@@ -75,23 +80,73 @@ class CausalDiscoveryEnsemble:
                             edge_counts[edge_key] += 1
                         else:
                             edge_counts[edge_key] = 1
-                            # Use the new nodes when adding edges
                             edge = Edge(
                                 new_nodes[i], new_nodes[j], Endpoint.TAIL, Endpoint.TAIL
                             )
                             union_graph.add_edge(edge)
 
-        # Create edge labels showing frequency
-        edge_frequency = {}
-        for (i, j), count in edge_counts.items():
-            frequency = count / len(self.algorithms)
-            edge_frequency[(i, j)] = f"{frequency:.2f}"
+        edge_frequency = self._get_frequency_from_count(
+            edge_counts,
+            len(self.algorithms),
+        )
 
-        # Save and return the union graph
-        self.est_graph_union = union_graph
         return union_graph, edge_frequency
 
-    def _create_filtered_graph(self, threshold: float = 0.5) -> GeneralGraph:
+    def _get_est_directed_union(self):
+        """
+        Create a graph that is the union of all directed edges from all algorithms with arrow frequency labels.
+
+        Returns:
+            Tuple[GeneralGraph, Dict]: A graph containing all directed edges that appear in at least
+                                      one algorithm's result, and a dictionary with direction frequencies
+        """
+        if not self.algorithms:
+            raise RuntimeError("No algorithms in the ensemble.")
+
+        if not self.is_ensemble_trained:
+            raise RuntimeError("Ensemble was not trained yet.")
+
+        orig_nodes = self.algorithms[0].est_graph_skeleton.get_nodes()
+        new_nodes = [GraphNode(node.get_name()) for node in orig_nodes]
+        directed_union = GeneralGraph(new_nodes)
+
+        arrow_counts = {}
+
+        for algo in self.algorithms:
+            graph = algo.est_graph
+            for i in range(len(orig_nodes)):
+                for j in range(len(orig_nodes)):
+                    if i == j:
+                        continue
+                    if (
+                        graph.get_endpoint(
+                            orig_nodes[i],
+                            orig_nodes[j],
+                        )
+                        == Endpoint.ARROW
+                    ):
+                        edge_key = (i, j)
+                        edge = Edge(
+                            new_nodes[i],
+                            new_nodes[j],
+                            Endpoint.TAIL,
+                            Endpoint.ARROW,
+                        )
+                        directed_union.add_edge(edge)
+
+                        if edge_key in arrow_counts:
+                            arrow_counts[edge_key] += 1
+                        else:
+                            arrow_counts[edge_key] = 1
+
+        edge_direction_frequency = self._get_frequency_from_count(
+            arrow_counts,
+            len(self.algorithms),
+        )
+
+        return directed_union, edge_direction_frequency
+
+    def _create_filtered_skeleton_graph(self, threshold: float = 0.5) -> GeneralGraph:
         """
         Create a filtered skeleton graph containing only edges with frequency above threshold.
 
@@ -111,23 +166,71 @@ class CausalDiscoveryEnsemble:
         if threshold < 0 or threshold > 1:
             raise ValueError("Threshold must be between 0 and 1")
 
-        # Create a new graph with the same nodes
-        nodes = self.est_graph_union.get_nodes()
+        nodes = self.est_skeleton_union.get_nodes()
         filtered_graph = GeneralGraph(nodes)
-        edge_labels = {}
+        filtered_edge_labels = {}
 
-        # Add only edges with frequency above threshold
         for (i, j), freq_str in self.edge_frequency.items():
             frequency = float(freq_str)
             if frequency >= threshold:
                 edge = Edge(nodes[i], nodes[j], Endpoint.TAIL, Endpoint.TAIL)
                 filtered_graph.add_edge(edge)
-                edge_labels[(i, j)] = freq_str
+                filtered_edge_labels[(i, j)] = freq_str
 
-        return filtered_graph, edge_labels
+        return filtered_graph, filtered_edge_labels
+
+    def _create_filtered_directed_graph(
+        self, threshold: float = 0.5
+    ) -> Tuple[GeneralGraph, Dict]:
+        """
+        Create a filtered directed graph containing only directed edges with frequency above threshold.
+
+        Parameters:
+        -----------
+        threshold : float
+            Minimum frequency required to include a directed edge (between 0 and 1)
+
+        Returns:
+        --------
+        Tuple[GeneralGraph, Dict]
+            A graph containing only directed edges above the threshold frequency,
+            and a dictionary with the frequency labels
+        """
+        if not self.is_ensemble_trained:
+            raise RuntimeError("Ensemble was not trained yet.")
+
+        if threshold < 0 or threshold > 1:
+            raise ValueError("Threshold must be between 0 and 1")
+
+        nodes = self.est_directed_union.get_nodes()
+        filtered_graph = GeneralGraph(nodes)
+        filtered_edge_labels = {}
+
+        for (i, j), freq_str in self.directed_frequency.items():
+            frequency = float(freq_str)
+            if frequency >= threshold:
+                edge = Edge(nodes[i], nodes[j], Endpoint.TAIL, Endpoint.ARROW)
+                filtered_graph.add_edge(edge)
+                filtered_edge_labels[(i, j)] = freq_str
+
+        return filtered_graph, filtered_edge_labels
+
+    @staticmethod
+    def _get_frequency_from_count(
+        count: dict,
+        denominator: int,
+    ) -> dict:
+        frequency = {}
+        for (i, j), count in count.items():
+            frequency[(i, j)] = round(count / denominator, 3)
+        return frequency
 
     def plot_ensemble_skeleton(
-        self, threshold: float = 0.5, title: str = None, fpath: Optional[str] = None
+        self,
+        type: str = "skeleton",
+        threshold: float = 0.5,
+        title: str = None,
+        fpath: Optional[str] = None,
     ):
         """
         Plot the ensemble skeleton with edges filtered by frequency threshold.
@@ -146,16 +249,22 @@ class CausalDiscoveryEnsemble:
         Figure
             Matplotlib figure object
         """
+        if type not in ("skeleton", "directed"):
+            raise ValueError(f"{type} is not a valid graph type.")
+
         if not title:
-            title = f"Ensemble Skeleton (threshold = {threshold})"
+            title = f"Ensemble {type} (threshold = {threshold})"
 
-        # Get filtered skeleton graph
-        filtered_graph, edge_labels = self._create_filtered_graph(threshold)
-
-        # Create edge labels dictionary for edges that are in the filtered graph
+        if type == "skeleton":
+            filtered_graph, edge_labels = self._create_filtered_skeleton_graph(
+                threshold
+            )
+        elif type == "directed":
+            filtered_graph, edge_labels = self._create_filtered_directed_graph(
+                threshold
+            )
 
         plotter = Plotter()
-
         fig = plotter.plot_labeled_graph(
             graph=filtered_graph, edge_labels=edge_labels, title=title, fpath=fpath
         )
