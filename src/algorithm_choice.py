4bca433ac94a89e2f469eb755d008ca024a08dac
DaviGiordano <davigiordano2704@gmail.com>
2025-03-31 21:46:40 +0100

    Add tetrad algorithm choice

--- src/algorithm_choice.py
+++ src/algorithm_choice.py
@@ -17,34 +17,37 @@ from src.causal_discovery.castle_algorithms import (
     GraNDAGAlgorithm,
 )
 
+from src.causal_discovery.tetrad_algorithms import PCTetrad
 
-def get_discovery_algorithm(
-    algorithm_name: str,
-    algorithm_params: dict,
-) -> CausalDiscoveryAlgorithm:
+
+def get_discovery_algorithm(**algorithm_config) -> CausalDiscoveryAlgorithm:
+    algorithm_name = algorithm_config.pop("algorithm_name", "")
+
     if algorithm_name == "pc":
-        return PCAlgorithm(algorithm_params)
+        return PCAlgorithm(algorithm_config)
     elif algorithm_name == "fci":
-        return FCIAlgorithm(algorithm_params)
+        return FCIAlgorithm(algorithm_config)
     elif algorithm_name == "ges":
-        return GESAlgorithm(algorithm_params)
+        return GESAlgorithm(algorithm_config)
     elif algorithm_name == "es":
-        return ExactSearchAlgorithm(algorithm_params)
+        return ExactSearchAlgorithm(algorithm_config)
     elif algorithm_name == "icalingam":
-        return ICALiNGAMAlgorithm(algorithm_params)
+        return ICALiNGAMAlgorithm(algorithm_config)
     elif algorithm_name == "directlingam":
-        return DirectLiNGAMAlgorithm(algorithm_params)
+        return DirectLiNGAMAlgorithm(algorithm_config)
     elif algorithm_name == "grasp":
-        return GRaSPAlgorithm(algorithm_params)
+        return GRaSPAlgorithm(algorithm_config)
     elif algorithm_name == "boss":
-        return BossAlgorithm(algorithm_params)
+        return BossAlgorithm(algorithm_config)
     elif algorithm_name == "notears":
-        return NOTEARSAlgorithm(algorithm_params)
+        return NOTEARSAlgorithm(algorithm_config)
     elif algorithm_name == "dag_gnn":
-        return DAGGNNAlgorithm(algorithm_params)
+        return DAGGNNAlgorithm(algorithm_config)
     elif algorithm_name == "corl":
-        return CORLAlgorithm(algorithm_params)
+        return CORLAlgorithm(algorithm_config)
     elif algorithm_name == "grandag":
-        return GraNDAGAlgorithm(algorithm_params)
+        return GraNDAGAlgorithm(algorithm_config)
+    elif algorithm_name == "pc_tetrad":
+        return PCTetrad(algorithm_config)
     else:
         raise NotImplementedError(f"{algorithm_name} was not yet implemented")
