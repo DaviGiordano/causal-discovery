1b78955d31d1fae7de153a7a2f69b5da52efa2c2
DaviGiordano <davigiordano2704@gmail.com>
2025-03-31 21:48:00 +0100

    Implement tetrad algorithm

---
+++ src/causal_discovery/tetrad_algorithms.py
@@ -0,0 +1,65 @@
+from src.graph_aux import dag_adj_to_graph
+from src.causal_discovery.CausalDiscoveryAlgorithm import CausalDiscoveryAlgorithm
+from src.pytetrad.TetradSearch import TetradSearch
+import pandas as pd
+import numpy as np
+import logging
+
+from src.parse_tetrad_string import (
+    str_to_chosen_edges,
+    str_to_edge_probabilities,
+    str_to_general_graph,
+)
+
+logger = logging.getLogger(__name__)
+
+INDEP_TEST_METHODS = {
+    "fisherz": "use_fisher_z",
+    "conditional_gaussian": "use_conditional_gaussian_test",
+    "degenerate_gaussian": "use_degenerate_gaussian_score",
+}
+
+
+class PCTetrad(CausalDiscoveryAlgorithm):
+    def __init__(self, config_params):
+        super().__init__(config_params)
+
+    def _set_indep_test(self, search):
+        algorithm_params = self.config_params.get("algorithm_params", {})
+        indep_test = algorithm_params.pop("indep_test", None)
+
+        try:
+            method_name = INDEP_TEST_METHODS[indep_test]
+            method = getattr(search, method_name)
+            method(**algorithm_params)
+
+        except KeyError:
+            raise NotImplementedError(
+                f"Independence test '{indep_test}' not found. "
+                f"Config params: {self.config_params}"
+            )
+
+    def _set_bootstrap(self, search):
+        bootstrap_params = self.config_params.get("bootstrap_params", {})
+        search.set_bootstrapping(**bootstrap_params)
+
+    def train(self, data: np.ndarray, node_names: list = []) -> None:
+        logger.info(f"Running PC in tetrad with params: {self.config_params}")
+
+        if not node_names:
+            node_names = [f"X{i+1}" for i in range(data.shape[1])]
+
+        df = pd.DataFrame(data, columns=node_names)
+        search = TetradSearch(df)
+
+        self._set_indep_test(search)
+        if self.config_params.get("bootstrap_params"):
+            self._set_bootstrap(search)
+
+        search.run_pc()
+
+        self.graph_string = str(search.get_string())
+        self.est_graph = str_to_general_graph(self.graph_string)
+        self.edge_probabilities = str_to_edge_probabilities(self.graph_string)
+        self.chosen_edges = str_to_chosen_edges(self.graph_string)
+        self._set_auxiliary_results()
