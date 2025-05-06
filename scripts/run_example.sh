5cfa44054f43cd4637b3b06ce10b725f98c451a4
DaviGiordano <davigiordano2704@gmail.com>
2025-05-06 16:25:45 +0100

    Remove some examples

--- scripts/run_example.sh
+++ scripts/run_example.sh
@@ -17,32 +17,32 @@ python3 main.py \
     --metadata data/example_continuous/metadata.json
 
     
-python3 main.py \
-    --config  "configs/fges.yaml" \
-    --data data/example_mixed/Xy_train.csv \
-    --output "output/example_output/fges/output.txt" \
-    --knowledge data/example_mixed/knowledge.txt \
-    --metadata data/example_mixed/metadata.json
+# python3 main.py \
+#     --config  "configs/fges.yaml" \
+#     --data data/example_mixed/Xy_train.csv \
+#     --output "output/example_output/fges/output.txt" \
+#     --knowledge data/example_mixed/knowledge.txt \
+#     --metadata data/example_mixed/metadata.json
 
 
-python3 main.py \
-    --config  "configs/grasp.yaml" \
-    --data data/example_mixed/Xy_train.csv \
-    --output "output/example_output/grasp/output.txt" \
-    --knowledge data/example_mixed/knowledge.txt \
-    --metadata data/example_mixed/metadata.json
+# python3 main.py \
+#     --config  "configs/grasp.yaml" \
+#     --data data/example_mixed/Xy_train.csv \
+#     --output "output/example_output/grasp/output.txt" \
+#     --knowledge data/example_mixed/knowledge.txt \
+#     --metadata data/example_mixed/metadata.json
 
 
-python3 main.py \
-    --config  "configs/pc.yaml" \
-    --data data/example_mixed/Xy_train.csv \
-    --output "output/example_output/pc/output.txt" \
-    --knowledge data/example_mixed/knowledge.txt \
-    --metadata data/example_mixed/metadata.json
+# python3 main.py \
+#     --config  "configs/pc.yaml" \
+#     --data data/example_mixed/Xy_train.csv \
+#     --output "output/example_output/pc/output.txt" \
+#     --knowledge data/example_mixed/knowledge.txt \
+#     --metadata data/example_mixed/metadata.json
 
-python3 main.py \
-    --config  "configs/dagma.yaml" \
-    --data data/example_continuous/Xy_train.csv \
-    --output "output/example_output/dagma/output.txt" \
-    --knowledge data/example_continuous/knowledge.txt \
-    --metadata data/example_continuous/metadata.json
+# python3 main.py \
+#     --config  "configs/dagma.yaml" \
+#     --data data/example_continuous/Xy_train.csv \
+#     --output "output/example_output/dagma/output.txt" \
+#     --knowledge data/example_continuous/knowledge.txt \
+#     --metadata data/example_continuous/metadata.json
