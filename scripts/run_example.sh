59e7ee280f6e501074b71cd25e35738143bfa7fa
DaviGiordano <davigiordano2704@gmail.com>
2025-05-06 14:24:51 +0100

    Update scripts

---
+++ scripts/run_example.sh
@@ -0,0 +1,6 @@
+python3 main.py \
+    --config  "configs/pc.yaml" \
+    --data data/example_data/Xy_train.csv \
+    --output "output/example_output/output.txt" \
+    --knowledge data/example_data/knowledge.txt \
+    --metadata data/example_data/metadata.json
